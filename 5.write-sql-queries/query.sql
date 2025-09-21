6af10372010fde70a7ba9f6849d7394fc5b835a2
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-22 01:25:44 +0530

    WHERE clause

--- 5.write-sql-queries/query.sql
+++ 5.write-sql-queries/query.sql
@@ -1,2 +1,6 @@
 -- SELECT * FROM cars;
-SELECT brand,model,condition,year FROM cars;
+-- SELECT brand,model,condition,year FROM cars;
+-- SELECT brand,model,color,price FROM cars
+-- WHERE color = 'black';
+SELECT brand,color,condition,model FROM cars
+WHERE condition = 0;
