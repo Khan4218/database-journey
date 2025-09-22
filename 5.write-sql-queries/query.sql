c2dc1786f6eed869d35952c86d192f667ebd41b2
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-22 15:16:39 +0530

    Nor Equal

--- 5.write-sql-queries/query.sql
+++ 5.write-sql-queries/query.sql
@@ -4,5 +4,20 @@
 -- WHERE color = 'black';
 -- SELECT brand,color,condition,model FROM cars
 -- WHERE condition = 0;
-SELECT brand,model,condition,price FROM cars
-WHERE price < 60000;
+-- SELECT brand,model,condition,price FROM cars
+-- WHERE price < 60000;
+/*
+	Filter out cars from 1965
+		Select the brand, model, year and price
+*/
+
+-- SELECT brand,model,year,price FROM cars
+-- WHERE year != 1965;
+
+/*
+	Find cars which are not yellow
+		Select the brand, model, price and color
+*/
+
+SELECT brand,model,price,color FROM cars
+WHERE color != 'yellow'
