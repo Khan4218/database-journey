d2640db4d3eebf03f4513b5e485433a06f9c31cb
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-22 15:07:39 +0530

    Numerical Filtering

--- 5.write-sql-queries/query.sql
+++ 5.write-sql-queries/query.sql
@@ -2,5 +2,7 @@
 -- SELECT brand,model,condition,year FROM cars;
 -- SELECT brand,model,color,price FROM cars
 -- WHERE color = 'black';
-SELECT brand,color,condition,model FROM cars
-WHERE condition = 0;
+-- SELECT brand,color,condition,model FROM cars
+-- WHERE condition = 0;
+SELECT brand,model,condition,price FROM cars
+WHERE price < 60000;
