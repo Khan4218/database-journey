c4dba2affbe8dca26451e5d88d3321073ff8fea0
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-24 17:27:37 +0530

    OR Operator

--- 5.write-sql-queries/query.sql
+++ 5.write-sql-queries/query.sql
@@ -85,7 +85,20 @@
 		and the color contains red
 */
 
-SELECT brand,model,condition,color,price FROM cars
-WHERE price BETWEEN 20000 AND 60000 
-AND condition BETWEEN 1 AND 3 
-AND color LIKE '%red%';
+-- SELECT brand,model,condition,color,price FROM cars
+-- WHERE price BETWEEN 20000 AND 60000 
+-- AND condition BETWEEN 1 AND 3 
+-- AND color LIKE '%red%';
+
+/*
+	Search for columns: brand, model, color, year, price, sold
+		from the table cars
+		where the color is a shade of red
+		or the year is between 1960 and 1969
+		and sold is false
+*/
+
+SELECT brand, model, color, year, price, sold FROM cars
+	WHERE (color LIKE '%red%'
+	OR year BETWEEN 1960 AND 1969)
+	AND sold IS FALSE;
