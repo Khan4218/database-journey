9c99a613fb4d737c86b4e590c8ab426d8263c728
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-09-24 17:15:36 +0530

    BETWEEN

--- 5.write-sql-queries/query.sql
+++ 5.write-sql-queries/query.sql
@@ -56,7 +56,36 @@
 		and the year is before 1970
 */
 
-SELECT brand,model,year,condition,price FROM cars
-WHERE condition >= 3
-AND year < 1970
-AND price < 100000;
+-- SELECT brand,model,year,condition,price FROM cars
+-- WHERE condition >= 3
+-- AND year < 1970
+-- AND price < 100000;
+
+/*
+	Select cars made between 1980 and 1989
+		show the brand, model, year and price
+*/
+
+-- SELECT brand,model,year,price FROM cars
+-- WHERE year >= 1980
+-- AND year <= 1989; 
+
+/*
+	Select cars made between 1980 and 1989
+		show the brand, model, year and price
+*/
+
+-- SELECT brand,model,year,price FROM cars
+-- WHERE year BETWEEN 1980 AND 1989;
+
+/*
+	Select brand, model, condition, color and price from cars
+		where the price is between $20,000 and $60,000
+		and the condition is between 1 and 3
+		and the color contains red
+*/
+
+SELECT brand,model,condition,color,price FROM cars
+WHERE price BETWEEN 20000 AND 60000 
+AND condition BETWEEN 1 AND 3 
+AND color LIKE '%red%';
