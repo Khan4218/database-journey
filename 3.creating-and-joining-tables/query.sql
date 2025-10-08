d446096ee24b950c8ccdffccc21a22e79511e660
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-10-08 06:36:47 +0530

    Populating Tables

--- 3.creating-and-joining-tables/query.sql
+++ 3.creating-and-joining-tables/query.sql
@@ -1,4 +1,4 @@
 -- SELECT id, brand, model FROM cars ORDER BY id;
 -- SELECT * FROM dealerships;
--- SELECT * FROM staff;
-SELECT * FROM sold_cars;
+SELECT * FROM staff;
+-- SELECT * FROM sold_cars;
