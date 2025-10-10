102aacff3361cee5475eebeefb69da3b8aadd1ef
Khan4218 <mirakber4@gmail.com>
2025-10-10 01:31:17 -0500

    Aggregates

--- 3.creating-and-joining-tables/query.sql
+++ 3.creating-and-joining-tables/query.sql
@@ -62,5 +62,62 @@
 		matching seller with staff.id	
 */
 
-SELECT name,role,sold_price FROM staff
-FULL JOIN sold_cars ON seller = staff.id;
+-- SELECT name,role,sold_price FROM staff
+-- FULL JOIN sold_cars ON seller = staff.id;
+
+/*
+	Select the city and average car price
+	Round that car price to a whole number
+	
+	Only show dealerships which have cars
+	
+	Group by dealership city and state
+*/
+
+
+-- SELECT city, state, ROUND(AVG(price), 2) AS avg_price
+-- 	FROM cars
+-- 	LEFT JOIN dealerships D ON dealership_id = D.id
+-- GROUP BY city, state;
+
+
+/*
+	Select the name and role, alongside a total_sales:
+		this is the sum of sales by a member of staff
+	
+	Use staff as your left table and sold_cars as your right table
+	
+	Include a where clause to select only staff with the role 'Salesperson'
+	
+	Group by staff name and role
+	Order by the total_sales from high to low
+*/
+
+SELECT name, role, SUM(sold_price) AS total_sales
+	FROM staff S
+	LEFT JOIN sold_cars ON S.id = seller
+WHERE role = 'Salesperson'
+GROUP BY name, role
+ORDER BY total_sales DESC;
+
+
+/*
+	Select the city, state and
+		count the total number of cars in each dealership
+		alias the count as car_count
+	
+	Use cars as the left table, and dealerships as the right table
+		choosing a join which will show every dealership
+		
+	Include a condition to count unsold cars
+	
+	Group by dealership city and state
+	Order by the car_count
+*/
+
+
+SELECT D.city, D.state , COUNT(C.id) AS car_count FROM cars C
+RIGHT JOIN dealerships D ON C.dealership_id = D.id
+WHERE C.sold IS NOT TRUE
+GROUP BY D.city, D.state
+ORDER BY car_count;
