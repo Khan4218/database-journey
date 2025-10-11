2ca5942bad3016c3de8bcf8871408bf414d820b6
Khan4218 <mirakber4@gmail.com>
2025-10-10 22:56:47 -0500

    EXISTS AND NOT EXISTS

--- 4.Advance-logic-and-conditions/query.sql
+++ 4.Advance-logic-and-conditions/query.sql
@@ -61,12 +61,12 @@
 		where the price is less than all cars which are in average condition (3)
 */
 
-SELECT brand, model, condition, price
-  FROM cars
-WHERE price < ALL (
-  SELECT price FROM cars
-    WHERE condition = 3
-);
+-- SELECT brand, model, condition, price
+--   FROM cars
+-- WHERE price < ALL (
+--   SELECT price FROM cars
+--     WHERE condition = 3
+-- );
 
 
 /*
@@ -75,12 +75,12 @@ WHERE price < ALL (
 	Order the results by year
 */
 
-SELECT brand,model,year FROM cars
-WHERE year < ALL(
-  SELECT year FROM cars 
-  WHERE brand = 'Ford'
+-- SELECT brand,model,year FROM cars
+-- WHERE year < ALL(
+--   SELECT year FROM cars 
+--   WHERE brand = 'Ford'
  
-) ORDER BY year;
+-- ) ORDER BY year;
 
 
 /*
@@ -90,9 +90,51 @@ WHERE year < ALL(
 	order the results by city
 */
 
-SELECT brand,model,city,price FROM cars C
-JOIN dealerships D ON C.dealership_id = D.id
-WHERE price > ALL (
-  SELECT sold_price FROM sold_cars
-)
-ORDER BY city;
+-- SELECT brand,model,city,price FROM cars C
+-- JOIN dealerships D ON C.dealership_id = D.id
+-- WHERE price > ALL (
+--   SELECT sold_price FROM sold_cars
+-- )
+-- ORDER BY city;
+
+/*
+	Select colors of car where there has been a sale of that color car
+*/
+-- SELECT * FROM sold_cars;
+SELECT DISTINCT color FROM cars
+WHERE EXISTS (
+	SELECT 1 FROM sold_cars WHERE cars_id = cars.id
+);
+
+
+/*
+	Select the city, state and date established of dealerships
+		Where there are no existing cars in stock
+		
+	Format the date in 'YYYY-MM-DD' format using TO_CHAR()
+		and alias it as 'est'
+*/
+
+SELECT city, state, TO_CHAR(established, 'YYYY-MM-DD') AS est
+  FROM dealerships D
+  WHERE NOT EXISTS (
+    SELECT 1 FROM cars WHERE dealership_id = D.id
+  );
+
+	/*
+	Select the city and state of dealerships
+	Where there exists a car priced at more than $50,000
+	
+	Hint: you'll need to match cars(dealership_id) with dealerships(id)
+	and then check for car price in your subquery
+*/
+
+SELECT city, state
+FROM dealerships D
+WHERE EXISTS (
+SELECT 1 FROM cars
+WHERE cars.dealership_id = D.id
+AND price > 50000
+)AND EXISTS (
+    SELECT 1 FROM sold_cars SC where seller = s.id
+  );
